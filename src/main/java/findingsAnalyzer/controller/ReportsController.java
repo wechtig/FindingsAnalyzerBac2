70a58fabaaad29fb9c9c4ef3fd3719666d519c1e
wechtig <wechtitsch.lukas@gmail.com>
2020-06-19 14:38:56 +0200

    add image conversion

--- src/main/java/findingsAnalyzer/controller/ReportsController.java
+++ src/main/java/findingsAnalyzer/controller/ReportsController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Base64;
+
 @RestController
 public class ReportsController {
     ReportService reportService = new ReportService();
@@ -14,6 +19,27 @@ public class ReportsController {
         //            , @PathVariable String startDateStr, @PathVariable String  endDateStr
 
         // und body die bilder
-        reportService.sendReport(data);
+        String[] strings = data.split(",");
+        String extension;
+        switch (strings[0]) {//check image's extension
+            case "data:image/jpeg;base64":
+                extension = "jpeg";
+                break;
+            case "data:image/png;base64":
+                extension = "png";
+                break;
+            default://should write cases for more images types
+                extension = "jpg";
+                break;
+        }
+        //convert base64 string to binary data
+        byte[] dataimg = Base64.getDecoder().decode(strings[1]);
+        String path = "images\\test_image.gif";
+        File file = new File(path);
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+            outputStream.write(dataimg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
